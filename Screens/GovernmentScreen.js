818ecd9904d4f829fb097b2277482dba322eb500
Arccie <milkclub224@gmail.com>
2024-11-22 17:15:24 +0800

    e

--- Screens/GovernmentScreen.js
+++ Screens/GovernmentScreen.js
@@ -8,9 +8,8 @@ const GovernmentScreen = () => {
 
   return (
     <ScrollView style={{ flex: 1, backgroundColor: theme === 'dark' ? '#1A1A19' : '#F6FCDF' }}>
-      {/* PhilHealth Insurance Section */}
-      <View style={{ height: 20 }}></View> {/* Spacer instead of <br> */}
-      <View style={[GovernmentStyles.container, { backgroundColor: theme === 'dark' ? '#1A1A1A' : '#F6FCDF' }]}>
+     
+      <View style={{ height: 20 }}></View> 
         <View style={[GovernmentStyles.schemeContainer, { backgroundColor: theme === 'dark' ? '#2A2A2A' : '#fff' }]}>
           <Text style={[GovernmentStyles.schemeTitle, { color: theme === 'dark' ? '#fff' : '#000' }]}>Scheme Name: PhilHealth Insurance</Text>
           <Text style={[GovernmentStyles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}>Description:</Text>
@@ -23,7 +22,6 @@ const GovernmentScreen = () => {
           <Text style={[GovernmentStyles.value, { color: theme === 'dark' ? '#fff' : '#000' }]}>Register online or visit the nearest PhilHealth office.</Text>
         </View>
 
-        {/* SSS Retirement Benefit Section */}
         <View style={[GovernmentStyles.schemeContainer, { backgroundColor: theme === 'dark' ? '#2A2A2A' : '#fff' }]}>
           <Text style={[GovernmentStyles.schemeTitle, { color: theme === 'dark' ? '#fff' : '#000' }]}>Policy Name: SSS Retirement Benefit</Text>
           <Text style={[GovernmentStyles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}>Description:</Text>
@@ -34,7 +32,7 @@ const GovernmentScreen = () => {
           <Text style={[GovernmentStyles.value, { color: theme === 'dark' ? '#fff' : '#000' }]}>Contributions vary based on salary and employment status.</Text>
           <Text style={[GovernmentStyles.label, { color: theme === 'dark' ? '#fff' : '#000' }]}>Contact Information:</Text>
           <Text style={[GovernmentStyles.value, { color: theme === 'dark' ? '#fff' : '#000' }]}>Visit the nearest SSS office or call the SSS hotline for assistance.</Text>
-        </View>
+        
       </View>
     </ScrollView>
   );
